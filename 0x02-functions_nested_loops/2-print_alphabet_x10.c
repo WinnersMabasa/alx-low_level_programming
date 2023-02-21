6159ba79159d017918ef7ec3fb7a02c900d2a3c9
Winners <90276025+WinnersMabasa@users.noreply.github.com>
2023-02-22 00:10:18 +0200

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,24 @@
 
 /**
  * print_alphabet_x10 - Check description
- * Description: It prints 10 times the alphabet, in lowercase, followed by a new line
+ * Description: It prints 10 times
  * Return: Nothing.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i;
 int x;
+<<<<<<< HEAD
 for(x=0;x < 10 ;x++)
+=======
+for (x = 0; x < 10 ; x++)
+>>>>>>> 80e12e64344ced165a1cff86ef70abbd3c60b7b7
+{
 for (i = 'a'; i <= 'z'; i++)
+{
 _putchar(i);
+}
 _putchar('\n');
+}
 }
